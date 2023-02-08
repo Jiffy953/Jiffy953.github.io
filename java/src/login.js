2a0f29c55b0ebb809d10341c3cfc69732d98b9f1
jiffy <jareds2147@gmail.com>
2023-02-07 21:34:39 -0500

    generaterandomstring

--- java/src/login.js
+++ java/src/login.js
@@ -1,3 +1,13 @@
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+  
 const element = document.getElementById("loginButton");
 element.addEventListener("click", loginButton);
 function loginButton() 
